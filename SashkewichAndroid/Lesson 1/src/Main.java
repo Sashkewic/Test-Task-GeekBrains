a5bec8025a05fffdf12c3c3cd90b6ed90f7409a4
Alexandr Zhivaykin <63651749+Sashkewic@users.noreply.github.com>
2022-10-01 17:49:23 +0600

    Add files via upload
    For Android

---
+++ SashkewichAndroid/Lesson 1/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1.
+        System.out.println("Вася");
+        System.out.println("    пошел");
+        System.out.println("        гулять");
+        System.out.println("---------------------------------------");
+
+        // Задание 2.
+        System.out.println("    $");
+        System.out.println("   $$$");
+        System.out.println("  $$$$$");
+        System.out.println(" $$$$$$$");
+        System.out.println("  HH HH");
+        System.out.println("  ZZZZZ");
+
+        // Домашнее задание - 1) Научиться вводить в строку, 2) Переменные в Java, 3) Хоткеи для IntelliJIdea и Windows, 4) Вызов командной строки
+        // Ресурсы для изучения Java - Метанит
+    }
+}
